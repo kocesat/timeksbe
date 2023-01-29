bb31fcefd0f6381bf9e0ed82f7479169a16af88d
kocesat <kocesat@gmail.com>
2023-01-29 22:01:14 +0300

    #REFACTOR(Esat) Clean imports

--- src/main/java/com/timeks/base/exception/IssueTypeNotFound.java
+++ src/main/java/com/timeks/base/exception/IssueTypeNotFound.java
@@ -1,7 +1,5 @@
 package com.timeks.base.exception;
 
-import com.timeks.base.exception.TimeksRuntimeException;
-
 public class IssueTypeNotFound extends TimeksRuntimeException {
     public static String MESSAGE = "Issue type code not found";
 
